7445c00bf75ddf29f53dbe6fc25a28869014e1f0
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-16 12:57:53 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int ll;
+	float f;
+
+	printf("size of a char: %ju byte(s)\i", sizeof(c));
+	printf("size of an int: %ju byte(s)\i", sizeof(i));
+	printf("size of a long int: %ju byte(s)\i", sizeof(l));
+	printf("size of a long long int: %ju byte(s)\i", sizeof(ll));
+	printf("size of a float: %ju byte(s)\i", sizeof(f);
+	return (0);
+}
