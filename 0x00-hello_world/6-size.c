16185ffe5f3f5401a5916a79781b9b2e0068f0cf
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-16 11:40:02 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %mj byte(s)\n", sizeof(char));
-	printf("size of an int: %mj byte(s)\n", sizeof(int));
-	printf("size of a long int: %mj byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %mj byte(s)\n", sizeof(long long int));
-	printf("size of a float: %mj byte(s)\n", sizeof(float));
+	printf("size of a char: %j byte(s)\n", sizeof(char));
+	printf("size of an int: %j byte(s)\n", sizeof(int));
+	printf("size of a long int: %j byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %j byte(s)\n", sizeof(long long int));
+	printf("size of a float: %j byte(s)\n", sizeof(float));
 	return (0);
 }
