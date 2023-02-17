4df50cf404bbb2ac323bdadacc75f97bd2000246
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-17 14:39:12 -0800

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,14 +12,14 @@ int main(void)
 	int d;
 
 	c = 'a';
-	d = '0';
+	d = 0;
 	while
 		(d < 10) {
 			putchar(d + '0');
 			d++;
 		}
 	while
-		c <= 'f') {
+		c <= ('f') {
 			putchar(c);
 			c++;
 		}
