68e6e2a44035afb5fa834bcb5f49fb53c9f6d69a
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-17 13:02:02 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char m;
+
+	char n;
+
+	m = 'a';
+	n = 'A';
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+	while
+		(n <= 'Z') {
+			putchar(n);
+			n++;
+		}
+	return (0);
+}
