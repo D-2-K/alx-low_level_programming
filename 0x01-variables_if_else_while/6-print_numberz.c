0d3893dcc1184232d6199fec28be28a9894db2f6
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-17 13:58:31 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result: Always 0
+ */
+int main(void)
+{
+	int b;
+
+	b = 0;
+	while
+		(b < 10) {
+			putchar(b + '0');
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
