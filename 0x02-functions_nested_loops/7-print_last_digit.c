59d5c14d5b76b2197a00de03918f65a84b192bc2
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 14:54:08 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,11 @@
  */
 int print_last_digit(int r)
 {
-	int ld = r % 10;
+	int x;
 
-	if (ld < 0)
-		ld *= -1;
-
-	_putchar(ld + '0');
-
-	return (0);
+	if (r < 0)
+	r = -r;
+	x = r % 10;
+	_putchar(x + '0');
+	return (x);
 }
