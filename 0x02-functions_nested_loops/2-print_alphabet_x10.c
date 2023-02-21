a97326c1788f1caab101ac57e43b6898c91e4b02
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 11:27:05 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,15 +3,18 @@
 /**
  * main - print lowercase alphabet ten times
  */
+
 void print_alphabet_x10(void);
 {
 	int x;
 	int a;
 
-	for (x = 1; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
+		{
 			_putchar(a);
+		}
 		_putchar('\n');
 	}
 }
