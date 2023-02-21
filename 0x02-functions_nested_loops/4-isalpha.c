5a6c33e91ed0d573d4cb4aa95796c27ce8a5b16f
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 12:41:59 -0800

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if its an alphabet
+ * @c: a character argument
+ *
+ * Returns: 1 if an alphabet,0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
