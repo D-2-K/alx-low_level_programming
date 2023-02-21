8c2c2eedf93b86abf74875daebcea107f2675aca
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 13:44:58 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of integer
+ * @a: parameter
+ * Return: absolute value of number
+ */
+
+int _abs(int a)
+{
+	int a;
+
+	a = abs(5);
+	_putchar("value of a = %d\n", a);
+
+	return (0);
+}
