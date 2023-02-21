801726baa5d3eef415ab18bbaee4d0ffb111d9aa
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 13:09:11 -0800

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - finds the value of character
+ * @n: argument passed
+ * Return: a sign as per set conditions
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
