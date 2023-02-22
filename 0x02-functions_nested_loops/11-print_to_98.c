1e4270831e0cda5e74e7a12f369c23771fca566f
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 16:22:06 -0800

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number to begin with
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+
+
+}
