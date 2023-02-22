cbf7dde3455d7cef1b761461bafe19148258bc5e
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-21 16:09:42 -0800

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two values and provide answer
+ * @a: first number
+ * @b: subsequent number
+ * Return: value
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
