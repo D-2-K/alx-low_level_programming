1ac455127056f392386eeb027aa0e76a30f5326b
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-23 11:49:53 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number as specified
+ *
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a == 2 || a == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a + '0');
+		}
+	}
+	_putchar('\n');
+}
