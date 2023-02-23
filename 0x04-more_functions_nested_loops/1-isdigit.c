d6b9172e46579618c5805d6a2a85f819d41d927a
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-23 09:37:25 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits in a specified range
+ * @c: input digit
+ * Return: 1 if it is a digit on the specified range and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}
