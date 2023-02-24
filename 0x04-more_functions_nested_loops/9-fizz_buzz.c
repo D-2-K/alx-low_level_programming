c369a028b9d52937d18df209682e3b5ab5f7df08
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-02-24 15:45:56 -0800

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz\t");
+		if (i == 100)
+			printf("Buzz");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz\t");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz\t");
+			printf("Buzz");
 		else
-			printf("%d\t", i);
+			printf("%d ", i);
 	}
+	printf("\n");
+
 	return (0);
 }
