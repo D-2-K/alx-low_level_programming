cab7f4eb2fb34e68c15c7599798244bb68e3bb8f
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-03-06 12:06:49 -0800

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,5 +11,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	unsigned char *memory = s, value = x
+	unsigned char *memory = s, value = b;
+	
+	for (i = 0; i < n; i++)
+		memory[i] = value;
+	return (memory);
 }
