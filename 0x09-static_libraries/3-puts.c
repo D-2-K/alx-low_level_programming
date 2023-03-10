b47c5368ee314656d21665eb539beeed71c985ca
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-03-10 10:55:30 -0800

    9

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,7 @@
 #include "main.h"
-void _puts(char *str)
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: always 0
- */
 
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts(s);
 }
