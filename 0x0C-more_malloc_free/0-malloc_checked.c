ab3f545af9870f7b01da20a27594e438302a635a
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-03-16 12:09:17 -0700

    Malloc_free2

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: size allocated in bytes
+ *
+ * Return: Pointer to allocated memory for success and
+ *         status value of 98 for fail
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *x;
+
+	x = mallloc(b);
+	if (x == NULL)
+		exit(98);
+	return (x);
+}
