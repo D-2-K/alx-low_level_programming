0e61b673d5379578fb8600686d34c63b77a27f63
Daniel Kinyua <danielkinyuakinoti@gmail.com>
2023-03-20 14:59:09 -0700

    Hotfix_2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,12 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_a;
-void init_dog(struct dog *x, char *name, float age, char *owner);
-void print_dog(struct dog *x);
-dog_a *new_dog(char *name, float age, char *owner);
-void free_dog(dog_a *x);
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif /*DOG_H*/
